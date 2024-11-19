af1b70f5786003fea89ccb75afac443b7951c96b
Akshay-Syscort <akshay.shaharwale@syscort.com>
2024-11-19 15:09:06 +0530

    dynamic fields

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,8 +1,15 @@
 import "./Button.css";
 function Button() {
+  let firstName = "Akshay";
+  const fullName = () => {
+    return "shaharwale";
+  };
+
   return (
     <div>
       <h1>from buton</h1>
+      <h2>my name is {firstName}</h2>
+      <h2>my last name is {fullName()}</h2>
     </div>
   );
 }
