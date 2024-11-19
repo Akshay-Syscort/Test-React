a48e6e3c61841b1ff3b26e199848b71ef9999e31
Akshay-Syscort <akshay.shaharwale@syscort.com>
2024-11-19 15:46:18 +0530

    bootstratp added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,41 @@ function App() {
   return (
     <>
       <Button></Button>
+      <button className=""></button>
+      <select class="form-select" aria-label="Default select example">
+        <option selected>Open this select menu</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+      </select>
+      <button type="button" class="btn btn-primary">
+        Primary
+      </button>
+      <button type="button" class="btn btn-secondary">
+        Secondary
+      </button>
+      <button type="button" class="btn btn-success">
+        Success
+      </button>
+      <button type="button" class="btn btn-danger">
+        Danger
+      </button>
+      <button type="button" class="btn btn-warning">
+        Warning
+      </button>
+      <button type="button" class="btn btn-info">
+        Info
+      </button>
+      <button type="button" class="btn btn-light">
+        Light
+      </button>
+      <button type="button" class="btn btn-dark">
+        Dark
+      </button>
+
+      <button type="button" class="btn btn-link">
+        Link
+      </button>
     </>
   );
 }
